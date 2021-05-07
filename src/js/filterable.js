d13ce41197c0fbaa5c153e748b775fc8f5a9ad6e
Samson Yuwono <samson.yuwono@gmail.com>
2021-05-07 00:38:47 -0400

    first draft completed

--- src/js/filterable.js
+++ src/js/filterable.js
@@ -1,17 +1,14 @@
 import "../styles/index.css";
 import { render } from "ejs";
 
-console.log("App Ready");
 function buildCorsFreeUrl(target) {
   return `https://cors.bridged.cc/${target}`;
 }
 
 let mediaList;
-let query;
+const mediaContainer = document.querySelector(".media-container");
 
-//extract fetch to another file
 function fetchMedia() {
-  // let mediaList;
   const apiUrl =
     "https://hubspotwebteam.github.io/CodeExercise/src/js/data/data.json";
 
@@ -27,85 +24,117 @@ function fetchMedia() {
       mediaList = data.media;
       renderList(mediaList, mediaContainer);
       searchFilter();
+      radioButtonFilter();
+      getCheckboxes();
+      clearAllFilters();
     })
     .catch((error) => {
       console.log(error);
     });
 }
-//fix querySelector vs getElementById
-const mediaContainer = document.querySelector("#media-container");
-// const radioButtons = document.querySelectorAll(".radio-button");
 
 const radioFilter = document.querySelector(".radio-filter");
 
-radioFilter.addEventListener("change", (e) => {
-  console.log(mediaList);
-  let radioFiltered = mediaList.filter((media) => {
-    return media.type == event.target.value;
+function radioButtonFilter() {
+  radioFilter.addEventListener("change", (e) => {
+    console.log(mediaList);
+    let radioFiltered = mediaList.filter((media) => {
+      return media.type == event.target.value;
+    });
+    console.log(radioFiltered);
+    renderList(radioFiltered, mediaContainer);
   });
-  console.log(radioFiltered);
-  renderList(radioFiltered, mediaContainer);
-});
+}
 
-const searchInput = document.getElementById("search-input");
+const searchInput = document.querySelector(".search-input");
 let searchQuery = "";
 
-//extract event listeners and consolidate all filters into 1 function
 function searchFilter() {
   searchInput.addEventListener("input", (e) => {
     searchQuery = e.target.value;
     console.log(searchQuery);
-    let filtered = mediaList.filter((media) => {
+    let searchFiltered = mediaList.filter((media) => {
       return media.title.toLowerCase().includes(searchQuery.toLowerCase());
     });
-    // console.log(searchQuery);
-    renderList(filtered, mediaContainer);
-
-    console.log("filtered", filtered);
+    renderList(searchFiltered, mediaContainer);
   });
+}
+
+let checkboxes = document.querySelector(".checkboxes");
+let selectBox = document.querySelector(".select-box");
+
+function showCheckboxes() {
+  let expanded = false;
+  selectBox.addEventListener("click", () => {
+    if (!expanded) {
+      checkboxes.style.display = "block";
+      expanded = true;
+    } else {
+      checkboxes.style.display = "none";
+      expanded = false;
+    }
+  });
+}
+
+showCheckboxes();
+
+let genreCheckboxes = document.querySelectorAll(".genre-checkbox");
+let selectedGenres = [];
+
+function getCheckboxes() {
+  genreCheckboxes.forEach((checkbox) => {
+    checkbox.checked = false;
+    checkbox.addEventListener("change", () => genreFilter());
+  });
+}
+
+function grabCheckboxValues() {
+  genreCheckboxes.forEach((checkbox) => {
+    if (checkbox.checked) selectedGenres = [...selectedGenres, checkbox.value];
+  });
+  selectedGenres = [...new Set(selectedGenres)];
+  console.log(selectedGenres);
+  return selectedGenres;
+}
+
+function genreFilter() {
+  selectedGenres = grabCheckboxValues();
+  let genreFiltered = mediaList.filter((media) => {
+    let genres = media.genre;
+    let result = (arr, target) => target.every((v) => arr.includes(v));
+    return result(genres, selectedGenres);
+  });
+
+  renderList(genreFiltered, mediaContainer);
 }
 
 const clearFilter = document.querySelector(".clear-filter");
 
-clearFilter.addEventListener("click", () => {
-  renderList(mediaList, mediaContainer);
-  document.querySelector("#search-input").value = "";
-  document.querySelector(".radio-filter").children[0].checked = false;
-  document.querySelector(".radio-filter").children[2].checked = false;
-});
+function clearAllFilters() {
+  clearFilter.addEventListener("click", () => {
+    renderList(mediaList, mediaContainer);
+    searchInput.value = "";
+    radioFilter.children[0].checked = false;
+    radioFilter.children[2].checked = false;
+    getCheckboxes();
+    selectedGenres = [];
+  });
+}
 
-//extract to another file
-function renderList(list, container, query) {
-  console.log("container", container);
+function renderList(list, container) {
   container.innerHTML = "";
-  console.log("list", list);
   let result = "";
-  console.log(query);
-  list.sort((a, b) => (a.title > b.title ? 1 : -1));
-  list.forEach((media) => {
-    result += `<div class="media-card">
+  list
+    .sort((a, b) => (a.title > b.title ? 1 : -1))
+    .forEach((media) => {
+      result += `<div class="media-card">
                  <img class="media-image" src="${media.poster}" alt=${media.title}/>
                   <p class="media-title">${media.title} (${media.year})</p>
                   <p class="media-genre">Genres: ${media.genre}</p>
                </div>`;
-  });
+    });
 
   container.innerHTML = result;
 }
 
 fetchMedia();
-
-// let expanded = false;
-// console.log(expanded);
-// function showCheckboxes() {
-//   let checkboxes = document.getElementById("checkboxes");
-//   if (!expanded) {
-//     checkboxes.style.display = "block";
-//     expanded = true;
-//   } else {
-//     checkboxes.style.display = "none";
-//     expanded = false;
-//   }
-// }
-
-// showCheckboxes();
--- submission.md
+++ submission.md
@@ -4,6 +4,20 @@ These notes will be read by HubSpot developers. Drop us a line!
 
 ## Given more time, what would you have done differently?
 
+1. I would utilize SCSS. I have exposure to SCSS but didn't feel comfortable using and applying my lack of knowledge in SCSS best practices for this.
+
+2. Added the filter for Year. I decided to focus on prioritizing the other filters in order to make it the best it possibly can. I would look into creating a slider for Year to switch things up.
+
+3. Fix the data structure for genres when it's being added to the array.
+
+4. Extract the fetchMedia function into a different file so I can keep things DRY and modular.
+
+5. Extract all event listeners for the filter section and consolidate it all into one function.
+
+6. Add an animation where the karat for the dropdown changes direction everytime it's toggled.
+
+7. Better error handling.
+
 _your answer here_
 
 ## How did you deviate from the directions, if at all, and why?
@@ -12,4 +26,5 @@ _your answer here_
 
 ## Is there anything else you'd like to let us know?
 
-_your answer here_
+I was coming back from a vacation and didn't have the benefit of completing this at my own leisure during the weekend.
+I wish I had more time to complete this. Please see the section above for what I would have done differently
